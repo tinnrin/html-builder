abb83b5367a9b9a502ec8859490a3ba1b8389659
tinnrin <tinnrin@gmail.com>
2024-01-20 22:19:19 +0300

    feat: implement task 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,46 @@
+//Прим.: в некоторых версиях bash существует баг, если при нажатии `ctrl + c` сообщение о выходе не показывается - попробуйте проверить через другую командную строку (cmd/powershell)
+
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const readline = require('readline');
+const os = require('os');
+
+const text = {
+  bold: '\x1b[1m',
+  reset: '\x1b[0m',
+};
+
+const fileName = './output.txt';
+const filePath = path.resolve(__dirname, fileName);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+process.stdout.write(`${text.bold}Enter your text:${text.reset}${os.EOL}`);
+
+const handleError = (error) => {
+  if (error) throw error;
+};
+
+const writeData = (input) => {
+  fs.appendFile(filePath, input + os.EOL, handleError);
+};
+
+const checkInput = (input) => {
+  if (input.trim().toLowerCase() === 'exit') {
+    rl.close();
+  } else {
+    writeData(input);
+  }
+};
+
+rl.on('line', (input) => checkInput(input));
+
+const handleClose = () => {
+  process.stdout.write(`${text.bold}Bye!${text.reset}`);
+};
+
+rl.on('close', handleClose);
